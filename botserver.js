b1a8270c788283bc69df8f854ec1cbbc0a4f7c7d
Minh Quang <tranminhquang42@gmail.com>
2025-10-03 18:15:29 +0700

    Bot server

---
+++ botserver.js
@@ -0,0 +1,12 @@
+{
+  "name": "discord-bot",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.15.3",
+    "express": "^4.19.2"
+  }
+}
